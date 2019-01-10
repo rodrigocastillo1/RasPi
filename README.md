6317b40acd5fd9e90ddd8007980fa94ba30d974a
Rodrigo Castillo Alcántara <castillo_15zs@hotmail.com>
2019-01-09 20:14:58 -0600

    Bot de Telegram RasPi.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RasPi
 Proyecto RasPi. Bot para Telegram diseñado para el curso de Raspberry Pi 2019-1 de UNAM PROTECO.
+
+Instalaciones necesarias para que funcione el bot:
+
+pip3 install python-telegram-bot
+pip3 install googletrans
---
+++ RasPi.py
@@ -0,0 +1,89 @@
+import telegram 
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+import time # Librería para hacer que el programa que controla el bot no se acabe.
+#from quotes import random
+from googletrans import Translator
+import random
+
+ 
+# Aqui definiremos aparte del Token, por ejemplo los ids de los grupos y pondríamos grupo= -XXXXX 
+ 
+TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' # Nuestro token del bot.
+AYUDA = 'Puedes utilizar los siguientes comandos : \n\n/ayuda - Guia para utilizar el bot. \n/info - Informacion De interes \n/hola - Saludo del Bot \n/piensa3D - Informacion sobre Piensa3D \n\n'
+LENGUAJES = 'Los siguientes lenguajes están disponibles : \n\nEspañol \nInglés \nFrancés \nAlemán \nJaponés \nItaliano \n\n Ingresa: /language seguido del idioma \n\n'
+languages_dict = {"Español": 'es', "Inglés": 'en', "Francés": 'fr', "Japonés": 'ja', "Italiano": 'it', "Alemán": 'de'}
+
+bot = telegram.Bot(token=TOKEN) # Creamos el objeto de nuestro bot.
+update = Updater(token=TOKEN)
+dispatcher = update.dispatcher
+trans = Translator()
+language = 'es'
+
+
+def helloTel(bot, update):
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="¡Hola! Estoy a tu servicio")
+
+def helpTel(bot, update): # Definimos una función que resuleva lo que necesitemos.
+    bot.send_chat_action(chat_id=update.message.chat_id, action='typing') # Enviando ...
+    time.sleep(1) #La respuesta del bot tarda 1 segundo en ejecutarse
+    bot.send_message(chat_id=update.message.chat_id, text=AYUDA) # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+
+def quoteTel(bot, update): # Definimos la función que enviará una cita random.
+    file = open("quotes.txt", "r", encoding="utf8")
+    quotes_list = file.read().split("\n\n")
+    quote = random.choice(quotes_list)
+    chat_id = update.message.chat_id # Guardamos el ID de la conversación para poder responder.
+    bot.send_chat_action(chat_id, 'typing') # Enviando ...
+    time.sleep(1) #La respuesta del bot tarda 1 segundo en ejecutarse
+    bot.send_message(chat_id, text=quote) # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+
+def translateTel(bot, update, args): # Definimos la función que enviará una cita random.
+    chat_id = update.message.chat_id # Guardamos el ID de la conversación para poder responder.
+    texto = ' '.join(args)
+    trans_text = trans.translate(texto, dest=language)
+    bot.send_chat_action(chat_id, 'typing') # Enviando ...
+    time.sleep(1) #La respuesta del bot tarda 1 segundo en ejecutarse
+    bot.send_message(chat_id, text=trans_text.text) # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+
+def languageTel(bot, update, args):
+    chat_id = update.message.chat_id
+    global language
+    print(''.join(args))
+    if not args:
+        bot.send_message(chat_id, text=LENGUAJES) # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+    else:
+        language = languages_dict.get(''.join(args))
+        mensaje = "Lenguaje cambiado a "+''.join(args)
+        bot.send_message(chat_id, text=mensaje)
+
+def grayTel(bot, update):
+    chat_id = update.message.chat_id
+    photo_file = bot.get_file(update.message.photo[-1].file_id)
+    response = requests.get(photo_file['file_path'])
+    img = Image.open(BytesIO(response.content))
+    img = img.convert('LA')
+    bio = BytesIO()
+    bio.name = 'image.png'
+    img.save(bio, 'PNG')
+    bio.seek(0)
+    bot.send_photo(chat_id, photo=bio)
+
+hello_handler = CommandHandler('hello', helloTel)
+help_handler = CommandHandler('help', helpTel)
+quote_handler = CommandHandler('quote', quoteTel)
+translate_handler = CommandHandler('translate', translateTel, pass_args=True)
+gray_handler = MessageHandler(Filters.photo, grayTel)
+language_handler = CommandHandler('language', languageTel, pass_args=True)
+dispatcher.add_handler(hello_handler)
+dispatcher.add_handler(help_handler)
+dispatcher.add_handler(quote_handler)
+dispatcher.add_handler(translate_handler)
+dispatcher.add_handler(gray_handler)
+dispatcher.add_handler(language_handler)
+
+update.start_polling()
+
